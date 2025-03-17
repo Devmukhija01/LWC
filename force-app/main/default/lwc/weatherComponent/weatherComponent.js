82751f39c00023096d96dfa874141f6f7ea0f67f
Dev Mukhija <Devmukhija999@gmail.com>
2025-03-17 15:58:33 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/weatherComponent/weatherComponent.js
@@ -0,0 +1,56 @@
+import { LightningElement, track } from 'lwc';
+import getWeatherData from '@salesforce/apex/WeatherController.getWeather';
+import getWeatherByCoordinates from '@salesforce/apex/WeatherController.getWeatherByCoordinates';
+
+export default class WeatherComponent extends LightningElement {
+    @track cityName = '';
+    @track weatherData;
+    @track currentWeatherData;
+    @track openGetWeather = false;
+    @track openCurrentGetWeather = false;
+
+    handleInputChange(event) {
+        this.cityName = event.target.value;
+    }
+
+    fetchWeather() {
+        this.openGetWeather=true;
+        this.openCurrentGetWeather=false;
+        if (this.cityName) {
+            getWeatherData({ cityName: this.cityName })
+                .then(result => {
+                    this.weatherData = JSON.parse(result);
+                    this.openGetWeather = true;
+                })
+                .catch(error => {
+                    console.error('Error fetching weather data', error);
+                });
+        }
+    }
+
+    fetchWeatherByLocation() {
+        this.openGetWeather=false;
+        this.openCurrentGetWeather=true;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                let latitude = position.coords.latitude;
+                let longitude = position.coords.longitude;
+                
+                getWeatherByCoordinates({ latitude: latitude.toString(), longitude: longitude.toString() })
+                    .then(result => {
+                        this.currentWeatherData = JSON.parse(result);
+                        this.openCurrentGetWeather = true;
+                    })
+                    .catch(error => {
+                        console.error('Error fetching weather by location', error);
+                    });
+            }, error => {
+                console.error('Error fetching location:', error);
+            });
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+    }
+
+    
+}
